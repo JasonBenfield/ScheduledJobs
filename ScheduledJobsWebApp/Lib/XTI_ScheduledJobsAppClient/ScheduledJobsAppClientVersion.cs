1e3dd6e062ada64d58b8f926e01ac42d59f1a4ca
jbenfield108@gmail.com <jbenfield108@gmail.com>
2022-08-12 15:57:50 -0400

    Version V10

--- ScheduledJobsWebApp/Lib/XTI_ScheduledJobsAppClient/ScheduledJobsAppClientVersion.cs
+++ ScheduledJobsWebApp/Lib/XTI_ScheduledJobsAppClient/ScheduledJobsAppClientVersion.cs
@@ -12,7 +12,7 @@ public sealed class ScheduledJobsAppClientVersion
         string value;
         if (hostEnv.IsProduction())
         {
-            value = "V9";
+            value = "V10";
         }
         else
         {
